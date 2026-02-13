2383143b2a58df3d125b6d5991727b149cb8d493
Idan Srur <office@hemy-hebrew.com>
2026-02-13 17:51:02 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 'use strict'
 
 var gBoard = []
-const MINE = '*'
+const MINE = '💣'
 const EMPTY = ''
+const EMPTYSLOT = '🔰'
+
 const FLAG = '⛳️'
+var gemeTimer
 
 var gBoardStatic = {
   minesAroundCound: 4,
@@ -15,28 +18,54 @@ var gBoardStatic = {
 var gLevel = {
   size: 4,
   mines: 2,
+  lives: 1,
 }
 var gGame = {
-  isOn: false,
+  isStart: false,
+  lives: 3,
   revealedCount: 0,
   markedCount: 0,
   secsPassed: 0,
 }
 
 function onInit() {
+  gGame = {
+    isStart: false,
+    revealedCount: 0,
+    markedCount: 0,
+    secsPassed: 0,
+  }
+  renderLife()
+  gBoard = []
   buildBoard()
   renderBoard()
-  setMinesNegsCount()
 }
 
-function setMinesNegsCount() {
+function changeSize(size) {
+  closePop()
+  var lives = 1
+  if (size >= 8) lives += 1
+  if (size >= 12) lives += 1
+
+  gLevel = {
+    size: size,
+    mines: size / 2,
+    lives: lives,
+  }
+  onInit()
+}
+
+function setMinesNegsCount(celPos) {
   for (let i = 0; i < gLevel.mines; i++) {
     var j = getRandomIntInclusive(0, gLevel.size - 1)
     var r = getRandomIntInclusive(0, gLevel.size - 1)
-    while (gBoard[j][r] === EMPTY) {
+    while (j === celPos.i && r === celPos.j) {
       gBoard[j][r] = MINE
+      j = getRandomIntInclusive(0, gLevel.size - 1)
+      r = getRandomIntInclusive(0, gLevel.size - 1)
     }
     gBoard[j][r] = MINE
+    console.log('i-j', j, r)
   }
 }
 
@@ -70,17 +99,22 @@ function renderBoard() {
 }
 
 function onCellClicked(elCell, i, j) {
-  const cellVal = gBoard[i][j]
   var celPos = {
     i,
     j,
   }
-  elCell.innerText = cellVal
-  if (cellVal === MINE) {
-  } else if (cellVal === FLAG) {
-  } else {
-    neighborCount(celPos)
+  if (!gGame.isStart) {
+    gGame.isStart = true
+    setMinesNegsCount(celPos)
+    gemeTimer = setInterval(startTime, 1000)
   }
+  console.log(gGame.isStart, gBoard)
+
+  const cellVal = gBoard[i][j]
+
+  elCell.innerText = cellVal
+
+  checkCell(celPos)
 }
 
 function neighborCount(celPos) {
@@ -96,9 +130,7 @@ function neighborCount(celPos) {
       }
     }
   }
-  console.log('work', celPos, count)
-
-  checkCell(celPos, count)
+  return count
 }
 
 function getRandomIntInclusive(min, max) {
@@ -107,19 +139,42 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled)
 }
 
-function expandReveal(cell) {}
-
 console.table(gBoard)
 
-function checkCell(cell, countNeig) {
-  var currCell = gBoard[cell.i][cell.j]
+function checkCell(cellPos) {
+  var currCell = gBoard[cellPos.i][cellPos.j]
+  var countNeig = neighborCount(cellPos)
 
   if (currCell === MINE) {
-    gameOver()
+    renderCell(cellPos, MINE)
+    stepBoom(cellPos)
   } else if (countNeig > 0) {
-    renderCell(cell, render)
+    renderCell(cellPos, countNeig)
   } else {
-    expandReveal(cell)
+    expandReveal(cellPos)
+  }
+}
+
+function stepBoom(cellPos) {
+  var elCell = document.querySelector(`.cell-${cellPos.i}-${cellPos.j}`)
+  gLevel.lives--
+  elCell.style.backgroundColor = '#c93e3eff'
+  renderLife()
+
+  if (gLevel.lives === 0) {
+    gameOver()
+  }
+}
+function gameOver() {
+  openPop()
+}
+
+function renderLife() {
+  var shild = document.querySelector('.shilds')
+  shild.innerHTML = ''
+  console.log(gLevel.lives)
+  for (let i = 0; i < gLevel.lives; i++) {
+    shild.innerHTML += '<div class="life"></div>'
   }
 }
 
@@ -129,14 +184,42 @@ function renderCell(cell, render) {
 }
 
 function expandReveal(cell) {
-  for (let i = celPos.i - 1; i <= celPos.i + 1; i++) {
+  for (let i = cell.i - 1; i <= cell.i + 1; i++) {
     if (i < 0 || i >= gBoard.length) continue
-    for (let j = celPos.j - 1; j <= celPos.j + 1; j++) {
+    for (let j = cell.j - 1; j <= cell.j + 1; j++) {
       if (j < 0 || j >= gBoard[i].length) continue
-      var currCell = gBoard[i][j]
-
-      renderCell(celPos, neighborCount(cell))
+      var currCell = { i, j }
+      var countNeig = neighborCount(currCell)
+      if (countNeig === 0) {
+        renderCell(currCell, EMPTYSLOT)
+      } else {
+        renderCell(currCell, countNeig)
+      }
     }
   }
-  console.log('work', celPos, count)
+}
+console.log(gBoard)
+
+function clickedRender() {
+  console.log('ddd')
+}
+function startTime() {
+  gGame.secsPassed += 1
+  const minutes = Math.floor(gGame.secsPassed / 60)
+  const seconds = gGame.secsPassed % 60
+  const formatted = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
+  const elTime = document.querySelector('.time')
+  if (elTime) elTime.innerText = formatted
+}
+
+function closePop() {
+  const pop = document.querySelector('.popNewGame')
+  pop.style.display = 'none'
+  console.log(pop, 'pop')
+  setInterval(startTime, 1000)
+}
+function openPop() {
+  var pop = document.querySelector('.popNewGame')
+  pop.style.display = 'grid'
+  setTimeout(gemeTimer)
 }
